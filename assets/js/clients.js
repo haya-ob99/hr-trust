ec6b0e402d0ae3416143e2b29eb4df2d0add1f4b
haya_alhosain <hayaalhosain.com@gmail.com>
2025-04-16 16:16:13 +0300

    add new pages

--- assets/js/clients.js
+++ assets/js/clients.js
@@ -1,21 +1,9 @@
-const contentData = {
+const allCards = document.querySelectorAll(".client-card");
+const allContentData = {
     1: {
         title: "Human Resources Transformation Project for",
         companyName: "Dr Abdulaziz Alajaji",
         section: "Health (private section)",
-        solution:
-            ["Attracting executives to a range of critical positions.", "Development of the operational model for business units.",
-                "Development of the organizational structure, task manuals, and business scope.",
-                "Functional analysis and matrix of functional groups and families.",
-                "Job descriptions.",
-                "Functional performance management system, policies, and forms, including all related components.",
-                "Framework and dictionaries for leadership, core, and technical competencies.",
-                "Comprehensive incentive framework and strategy, including job evaluation, job grade structure, salary scale, benefits, rewards, compensations, and financial impact study.",
-                "Career growth pathways framework and system.",
-                "Knowledge transfer workshops and focus groups.",
-                "Human capital aptitude tests and assessments.",
-                "Leadership training and development plans."
-            ],
         steps: [
             "Corporate organizational structure",
             "Compensation & benefits framework (salaries, incentives, rewards, and total remuneration packages)",
@@ -33,10 +21,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Leejam",
         section: "Sports (private section)",
-        solution:
-            ["Human Resources field.", "Supply Chain field.",
-                "Project Management field.", "Real Estate Management field.", "Consulting for the Board of Directors."
-            ],
         steps: ["Attracting Executives and Supervisors"],
         image: "./assets/images/service1.png",
         year: "2024 - 2025",
@@ -46,20 +30,20 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Water Transmission and technologies co.",
         section: "Desalination and water transportation (government section)",
-        solution:
-            ["Implementation of the employee transfer journey according to approved plans",
-                "Launch and activation of the digital job offers platform",
-                "Execution of the defined scope for change management and communication strategy, including",
-                "Launch and activation of the new employee orientation program, including",
-                "Activation of the Key Performance Indicators (KPIs) dashboard for the employee transfer phase"
-            ],
         steps: ["Project management and performance reports (Project Plan)",
             "Developing and implementing an employee transfer readiness assessment scale",
             "Conducting planning and alignment processes (job placement)",
             "Designing the employee transition journey, including:",
             "Developing the digital platform for job offers (automating offer mechanisms)",
             "Establishing a governance, risk and compliance framework (regulations, policies, procedures, and contracts)",
-            "Developing new employee orientation and support services program"
+            "Developing new employee orientation and support services program:"
+        ],
+        subSteps: [
+            "Conducting an organizational culture survey and analysis",
+            "Implementing change management and communication strategies, plans, and execution",
+            "Developing a digital platform for job offers (automated offer mechanisms)",
+            "Establishing a governance, risk, and compliance framework (including regulations, policies, procedures, and contracts)",
+            "Designing a new employee orientation and support services program"
         ],
         image: "./assets/images/service1.png",
         year: "2024 - 2025",
@@ -69,17 +53,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Esnad",
         section: "Industry and mining (government section)",
-        solution:
-            ["Job Satisfaction and Employee Engagement Report",
-                "Identification of gaps, improvement areas, and development opportunities",
-                "Results of workload analysis studies",
-                "Results of workforce analysis studies",
-                "Results of financial impact analysis for current state",
-                "Development of benefits, incentives, rewards, and compensation packages for the regulatory section",
-                "Benchmarking analysis of financial impact before and after development recommendations",
-                "General supervision of human capital section",
-                "Acting as Secretary General for Compensation and Nominations Committee"
-            ],
         steps: [
             "Study and measurement of employee satisfaction and engagement in the regulatory section",
             "Study of benefits, incentives, rewards, and compensation packages for the regulatory section",
@@ -95,14 +68,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Engineer Holding Group",
         section: "Investment, media, manufacturing, hospitality, and real estate (private section)",
-        solution:
-            ["Attracting a group of executive-level talents, such as the Chief Financial Officer (CFO).",
-                "A report on the search results for targeted talents.",
-                "A list of the top candidates for the specified positions.",
-                "Communications and job interviews with the nominated candidates.",
-                "Facilitating the recruitment process between the candidates and the client.",
-                "Following up on recruitment and hiring procedures beyond the probation period."
-            ],
         steps: [
             "Attraction and recruitment of talents.",
             "Executive talent acquisition."
@@ -113,16 +78,8 @@ const contentData = {
     },
     6: {
         title: "Human Resources Transformation Project for",
-        companyName: "Fuchsia",
+        companyName: "Fuchsia & Alhasa Auto Bakery",
         section: "baked goods and sweets (private section)",
-        solution:
-            ["Attracting a Chief Executive Officer (CEO) for Al-Ahsa Bakeries and Fauchier.",
-                "A report on the search results for targeted talents.",
-                "A list of the top candidates for the specified positions.",
-                "Communications and job interviews with the nominated candidates.",
-                "Facilitating the recruitment process between the candidates and the client.",
-                "Following up on recruitment and hiring procedures beyond the probation period."
-            ],
         steps: [
             "Attracting and recruiting talents.",
             "Attracting executives."
@@ -135,12 +92,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Masah",
         section: "Contracting (private section)",
-        solution:
-            ["Business Unit Operating Model Development",
-                "Organizational Structure Development, Task Manuals, and Business Scope",
-                "Functional Analysis & Matrix of Functional Groups and Families", "Job Descriptions", "Performance Management System, Policy, and Templates (Including All Related Components)",
-                "HR Policies and Procedures", "Framework and Dictionaries for Leadership, Core, and Technical Competencies", "Comprehensive Incentives Framework & Strategy", " Knowledge Transfer Workshops & Focus Groups"
-            ],
         steps: [
             "Corporate organizational structure.", "Wages, Benefits, Incentives, Perks, Rewards, and Compensations", "Functional Performance Management System (Including All Related Components)", "Functional Competencies."
         ],
@@ -152,14 +103,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "AgriServ",
         section: "Agriculture and services (government section)",
-        solution:
-            ["Attracting a group of talents at the executive level, such as the Chief Financial Officer (CFO).",
-                "A report on the results of the search for targeted talents.",
-                "A list of the most prominent candidates for the specified positions.",
-                "Communications and job interviews with the nominated candidates.",
-                "Facilitating the recruitment process between the candidates and the client.",
-                "Following up on the recruitment and hiring procedures beyond the probation period."
-            ],
         steps: ["Talent Acquisition (Executives & All Organizational Levels)"],
         image: "./assets/images/service1.png",
         year: "2024",
@@ -169,17 +112,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "CHERRY - CAR RENTAL",
         section: "Transportation and logistics services (private section)",
-        solution:
-            ["Development of the operating model for business units.",
-                "Development of the organizational structure, task manuals, and scope of work.",
-                "Job analysis and the matrix of job groups and families.",
-                "Job descriptions.",
-                "Performance management system, policy, and forms with all related components.",
-                "Human resources policies and procedures.",
-                "Frameworks and dictionaries for leadership, core, and technical competencies.",
-                "Comprehensive incentive framework and strategy, including job evaluation, job grading structure, salary scale, benefits, perks, bonuses, compensations, and financial impact study.",
-                "Knowledge transfer workshops and focus groups."
-            ],
         steps: [
             "Institutional organizational structure.",
             "Salaries, benefits, incentives, perks, bonuses, and compensations.",
@@ -194,24 +126,6 @@ const contentData = {
         title: "Human Resources Transformation Project for",
         companyName: "Almajdiah Residence",
         section: "Real estate (private section)",
-        solution:
-            ["Strategy review",
-                "Setting objectives and KPIs for executives and project owners",
-                "Strategic planning and alignment of objectives, initiatives, and projects",
-                "Corporate and individual performance scorecards",
-                "Communication and governance policy/plan for strategy implementation",
-                "Automation of project management and corporate performance tracking",
-                "Business units' operating model",
-                "Organizational structure with task manuals and scope of work",
-                "Functional analysis and matrix of functional groups/families",
-                "Roles and authorities of the Managing Director and executives",
-                "Job descriptions",
-                "Performance management system (policy, models, and all related components)",
-                "Framework and dictionaries for leadership, core, and technical competencies",
-                "Comprehensive incentive strategy framework including:",
-                "Career growth pathways framework and system",
-                "Knowledge transfer workshops and focus groups"
-            ],
         steps: [
             "Planning and monitoring implementation of the new strategy and corporate performance",
             "Current state assessment to establish baseline for initiatives and projects (Baseline)",
@@ -230,20 +144,7 @@ const contentData = {
     11: {
         title: "Human Resources Transformation Project for",
         companyName: "MCDC",
-        goal: ["Institutional transformation"],
-        solution:
-            ["Business unit operating model development",
-                "Organizational structure development with job manuals and scope of work",
-                "Job analysis and functional groups/matrix",
-                "Job descriptions",
-                "Performance management system (policy, framework, and all related components)",
-                " HR policies and procedures",
-                "Competency framework and dictionaries (leadership, core, and technical)",
-                "Comprehensive incentive strategy framework including:",
-                "Knowledge transfer workshops and focus groups",
-                "Career path design",
-                "Leadership succession planning"
-            ],
+        section: "Real estate and hotel management (public contribution)",
         steps: [
             "Corporate Organizational Structure",
             "Compensation & Benefits Framework",
@@ -255,35 +156,26 @@ const contentData = {
         year: "2025",
         duration: "6"
     },
-    12: {
-        title: "Human Resources Transformation Project for",
-        companyName: "Alhasa Auto Bakery",
-        section: "baked goods and sweets (private section)",
-        solution:
-            ["Attracting a Chief Executive Officer (CEO) for Al-Ahsa Bakeries and Fauchier.",
-                "A report on the search results for targeted talents.",
-                "A list of the top candidates for the specified positions.",
-                "Communications and job interviews with the nominated candidates.",
-                "Facilitating the recruitment process between the candidates and the client.",
-                "Following up on recruitment and hiring procedures beyond the probation period."
-            ],
-        steps: [
-            "Attracting and recruiting talents.",
-            "Attracting executives."
-        ],
-        image: "./assets/images/service1.png",
-        year: "2022",
-        duration: "4"
-    },
 };
 
-const updateContent = (swiper) => {
-    const activeSlide = swiper.slides[swiper.activeIndex];
-    if (!activeSlide) return;
-    const contentId = activeSlide.getAttribute("data-content-id");
-    const content = contentData[contentId];
-
+const updateContent = (content) => {
     if (content) {
+        const stepsContent =
+            content.steps.length === 1
+                ? `<p>${content.steps[0]}</p>`
+                : `<ul>${content.steps.map((step) => {
+                    let subStepsHTML = '';
+
+                    if (step === "Developing new employee orientation and support services program:" && content.subSteps) {
+                        subStepsHTML = `
+                            <ol>
+                                ${content.subSteps.map(sub => `<li>${sub}</li>`).join("")}
+                            </ol>`;
+                    }
+
+                    return `<li>${step}${subStepsHTML}</li>`;
+                }).join("")}</ul>`;
+
         document.querySelector(".client-content").innerHTML = `
         <div class="content">
           <h2>
@@ -292,19 +184,11 @@ const updateContent = (swiper) => {
           <span>Contract duration: ${content.duration} Months , Year : ${content.year}</span>
           <div class="box goal">
             <h4>Section:</h4>
-            <ul><li>${content.section}</li></ul>
-          </div>
-          <div class="box solution">
-            <h4>Most prominent outputs:</h4>
-            <ul>
-              ${content.solution.map((item) => `<li>${item}</li>`).join("")}
-            </ul>
+            <p>${content.section}</p>
           </div>
           <div class="box steps">
             <h4>Scope of Work:</h4>
-            <ul>
-              ${content.steps.map((step) => `<li>${step}</li>`).join("")}
-            </ul>
+            ${stepsContent}
           </div>
         </div>
         <div class="image">
@@ -314,27 +198,24 @@ const updateContent = (swiper) => {
     }
 };
 
-const swiper3 = new Swiper(".mySwiper3", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-    },
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 200,
-        modifier: 1,
-        slideShadows: false,
-    },
-    on: {
-        slideChange: function () {
-            updateContent(this);
-        },
-    },
+
+allCards.forEach((card) => {
+    card.addEventListener("click", () => {
+        const contentId = card.getAttribute("data-content-id");
+        const content = allContentData[contentId];
+
+        updateContent(content);
+
+        allCards.forEach((c) => c.classList.remove("client-card-active"));
+        card.classList.add("client-card-active");
+    });
 });
-updateContent(swiper3);
+
+window.addEventListener("DOMContentLoaded", () => {
+    const firstCard = allCards[0];
+    const firstId = firstCard.getAttribute("data-content-id");
+    const firstContent = allContentData[firstId];
+
+    updateContent(firstContent);
+    firstCard.classList.add("client-card-active");
+});
